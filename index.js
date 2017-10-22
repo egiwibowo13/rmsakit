f85d10f1cc561e5856aff2571a45d103daeb987d
egiwibowo13 <32433634+egiwibowo13@users.noreply.github.com>
2017-10-22 22:06:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ app.use('/',function(req,res,next){
     next();
 });
 
-let login = require('./login/loginRoute.js');
-app.use('/api',login);
 
 let petugasRoute = require('./petugas/petugasRoute.js');
 app.use('/api',petugasRoute);
